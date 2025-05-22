440f963d2273e51be639348c7f7fcc495fab5e77
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2025-05-22 17:52:09 +0530

    Add .next/static/chunks/src_app_compress_pdf_page_tsx_1ce284ae._.js 1747916527390

---
+++ .next/static/chunks/src_app_compress_pdf_page_tsx_1ce284ae._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_compress_pdf_page_tsx_dba61732._.js"
+],
+    source: "dynamic"
+});
