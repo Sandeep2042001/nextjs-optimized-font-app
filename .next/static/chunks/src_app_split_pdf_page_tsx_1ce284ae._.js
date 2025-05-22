d4b906d61ce50619f2bf2f1c7ad8c087edd0a55b
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2025-05-22 17:52:15 +0530

    Add .next/static/chunks/src_app_split_pdf_page_tsx_1ce284ae._.js 1747916532719

---
+++ .next/static/chunks/src_app_split_pdf_page_tsx_1ce284ae._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_split_pdf_page_tsx_58dd4f2e._.js"
+],
+    source: "dynamic"
+});
